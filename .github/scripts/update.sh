a0df4ab7d0c15673aa064ab14153ea0420fe6052
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2024-09-23 08:52:10 +0000

    update to version:

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-SONARR_URL="https://download.sonarr.tv/v4/main/"
+SONARR_URL="https://services.sonarr.tv/v1/releases"
 
-FULL_LAST_VERSION=$(curl -SsL ${SONARR_URL} | grep -Po '<a href="\K([\d.]+)' | grep -v "\.\." |  sort -t . -k 4 | tail -1)
+FULL_LAST_VERSION=$(curl -SsL ${SONARR_URL} | jq -r "first(.[] | select(.releaseChannel==\"${SONARR_CHANNEL}\") | .version)")
 LAST_VERSION="${FULL_LAST_VERSION}"
 
 if [ "${LAST_VERSION}" ]; then
